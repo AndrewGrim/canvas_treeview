f1f27defc6c68600283476a5c1135230cb54c546
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-08-22 10:30:40 +0100

    Added comments

--- src/treeview/enums.ts
+++ src/treeview/enums.ts
@@ -1,9 +1,11 @@
+// Specifies the alignment of cell contents within a column.
 export enum Alignment {
     Left,
     Right,
     Center
 }
 
+// Specifies the order by which to sort column values.
 export enum Sort {
     Ascending,
     Descending
