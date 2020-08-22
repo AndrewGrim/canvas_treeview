4488422fe2cc2d7d1e179f3bfb24d16ecc0c4f1d
AndrewGrim <39337035+AndrewGrim@users.noreply.github.com>
2020-08-22 12:53:49 +0100

    Add doc comments

--- src/weapons/mod.ts
+++ src/weapons/mod.ts
@@ -8,11 +8,14 @@ import {loadContent} from "./tree";
 import {TextCellRenderer, ImageCellRenderer} from "../treeview/cellrenderer";
 import {Alignment} from "../treeview/enums";
 
+// Entry point of the application.
 export function render(): void {
+    // Set default TreeView data layout.
     let use_tree_layout = true;
     let treeview = new TreeView();
+        // Set number of columns and use auto size.
         treeview.setColumnCount(9);
-        // treeview.setColumns([444, 50, 70, 50, 50, 60, 26, 26, 201]);
+        // Set CellRenderers for each column heading.
         treeview.setColumnHeadings(
             {
                 name: new TextCellRenderer("Name", Alignment.Center),
@@ -26,9 +29,11 @@ export function render(): void {
                 sharpness: new TextCellRenderer("Sharpness", Alignment.Center)
             }
         );
+        // Add callback function when a TreeView row is selected.
         treeview.bindOnRowSelected(loadDetailView);
     let db: Database = new sqlite3("mhwi.db");
     
+    // Added event handlers for all the weapon buttons.
     document.getElementById("great-sword-btn").addEventListener("click", (event) => { loadContent("great-sword", treeview, db, use_tree_layout); });
     document.getElementById("long-sword-btn").addEventListener("click", (event) => { loadContent("long-sword", treeview, db, use_tree_layout); });
     document.getElementById("sword-and-shield-btn").addEventListener("click", (event) => { loadContent("sword-and-shield", treeview, db, use_tree_layout); });
@@ -44,12 +49,15 @@ export function render(): void {
     document.getElementById("heavy-bowgun-btn").addEventListener("click", (event) => { loadContent("heavy-bowgun", treeview, db, use_tree_layout); });
     document.getElementById("bow-btn").addEventListener("click", (event) => { loadContent("bow", treeview, db, use_tree_layout); });
 
+    // Add event handler when typing in the search box.
     document.getElementById("weapon-search").addEventListener("input", (event) => { loadContent(null, treeview, db, use_tree_layout); });
 
     let tree_layout = document.getElementById("tree-layout-btn");
         tree_layout.classList.add("highlight");
     let table_layout = document.getElementById("table-layout-btn");
 
+    // Add event handlers for switching between different
+    // TreeView data layout modes.
     tree_layout.addEventListener("click", (event) => {
         tree_layout.classList.add("highlight");
         table_layout.classList.remove("highlight");
@@ -64,14 +72,19 @@ export function render(): void {
         use_tree_layout = false;
         loadContent(null, treeview, db, use_tree_layout);
     });
+
+    // Add event handlers to switch between weapon detail tabs.
     document.getElementById("detail-tab-btn").addEventListener("click", (event) => { showTab(event); });
     document.getElementById("ammo-tab-btn").addEventListener("click", (event) => { showTab(event); });
     document.getElementById("melodies-tab-btn").addEventListener("click", (event) => { showTab(event); });
 
+    // Load initial data.
     document.getElementById("detail-tab-btn").click();
     loadContent("great-sword", treeview, db, use_tree_layout);
 }
 
+// Adjusts the sharpness data and returns the sharpness scaled to the level of handicraft.
+// Used to display and draw the weapons sharpness at each level of handicraft.
 export function adjust_sharpness(sharpness: number[], maxed: boolean, handicraft_level: number, modifier: number = 10): number[] {
     if (maxed) return sharpness;
 
